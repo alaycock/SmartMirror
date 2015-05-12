e1d86c7a62df12a42da139d2ab0ef446d3c59308
Adam <adam.laycock@gmail.com>
2015-05-11 22:42:40 -0600

    Starting stuff

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
+// todo:
+// Routing for restarting the application, look into using nodemon for auto-restart
+// Make the page I want to serve
+//  - RSS feed
+//  - Weather
+//  - Time
+//  - Unread emails
+
 var connect = require('connect');
+var connectRoute = require('connect-route');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
-var thing = [3,2,3];
+
+// Serve static files
+var app = connect().use(serveStatic(__dirname + "/public"));
+
+// For dynamic pages
+app.use(connectRoute(function (router) {
+	router.get('/', function(req, res, next) {
+		console.log("Page hit");
+		res.end();
+	});
+}));
+
+app.listen(8080);
